103da953a1a335db3a65c493f6771d494f6ac8f0
Az4reus <az4reus@gmail.com>
2016-10-17 10:33:54 +0200

    Removed useless line-width field

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,9 @@
 pub struct Config {
     pub base_filepath: String,
-    pub line_width: i32,
 }
 
 pub fn default_config() -> Config {
     Config {
         base_filepath: "/Users/az/logs/".to_owned(),
-        line_width: 80,
     }
 }
